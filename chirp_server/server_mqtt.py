7b4b5f63875771ca0d4e8ea6590fea5fb803ea94
Felipe Faggion <felipe.faggion.ufsc@gmail.com>
2025-09-24 08:54:59 -0300

    deleted useless file, updated server to consider all nodes (2)

--- chirp_server/server_mqtt.py
+++ chirp_server/server_mqtt.py
@@ -2,38 +2,41 @@ import paho.mqtt.client as mqtt
 import base64
 import json
 
-APP_ID = "50981d4c-9ebd-49fc-a7d4-f88ea8598ef2"  # seu applicationId
-DEV_EUI = "70b3d57ed00717ae"
+APP_ID = "50981d4c-9ebd-49fc-a7d4-f88ea8598ef2"
+DEV_EUIs = ["70b3d57ed00717ae", "70b3d57ed0072a97"]
 
-UPLINK_TOPIC = f"application/{APP_ID}/device/{DEV_EUI}/event/up"
-DOWNLINK_TOPIC = f"application/{APP_ID}/device/{DEV_EUI}/command/down"
 
 BROKER = "localhost"
 PORT = 1883
 
 def on_connect(client, userdata, flags, reason_code, properties=None):
     print("Conectado ao broker MQTT!")
-    client.subscribe(UPLINK_TOPIC)
-    print(f"Assinado no tópico: {UPLINK_TOPIC}")
+
+    for dev_eui in DEV_EUIs:
+        uplink_topic = f"application/{APP_ID}/device/{dev_eui}/event/up"
+        client.subscribe(uplink_topic)
+        print(f"Assinado no tópico: {uplink_topic}")
 
 def on_message(client, userdata, msg):
-    print(f"\n📡 Uplink recebido no tópico {msg.topic}")
+    print(f"Uplink recebido no tópico {msg.topic}")
     data = json.loads(msg.payload.decode())
 
-    # Decodifica payload (caso exista)
+    parts = msg.topic.split("/")
+    dev_eui = parts[3]
+
     if "data" in data:
         payload_bytes = base64.b64decode(data["data"])
-        print(f"Payload bruto: {payload_bytes}")
+        print(f"Payload bruto de {dev_eui}: {payload_bytes}")
 
-    # Monta o downlink "Hello world"
     down_payload = {
         "confirmed": False,
         "fPort": 1,
         "data": base64.b64encode(b"Hello world").decode()
     }
 
-    print(f"🔽 Enviando downlink: {down_payload}")
-    client.publish(DOWNLINK_TOPIC, json.dumps(down_payload))
+    downlink_topic = f"application/{APP_ID}/device/{dev_eui}/command/down"
+    print(f"Enviando downlink para {dev_eui}: {down_payload}")
+    client.publish(downlink_topic, json.dumps(down_payload))
 
 def main():
     client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
@@ -41,10 +44,9 @@ def main():
     client.on_message = on_message
 
     client.connect(BROKER, PORT, 60)
-    print("Servidor iniciado, aguardando up links...\n")
+    print("Servidor iniciado, aguardando uplinks...\n")
 
     client.loop_forever()
 
 if __name__ == "__main__":
     main()
-
